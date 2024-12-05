fe5b5c14e04acf4cc77038a5f06b89da0aaf0592
brylin22 <bchavezc@educantabria.es>
2024-12-05 15:53:00 +0100

    Ejercicio realizado. Copia del 5/12/24 a las 15:51

--- src/main/java/Hoja_01/pruebagity.java
+++ src/main/java/Hoja_01/pruebagity.java
@@ -15,7 +15,7 @@ public class pruebagity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(" git ");
+        System.out.println(" gity ");
     }
     
 }
